60642d7713e1dcee5bd8147cd7d78f53b329fc7a
rockStar378 <shahzadluqman092@gmail.com>
2025-12-24 11:30:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -43,8 +43,8 @@ GIT_TOKEN = getenv(
 API_KEY = getenv("API_KEY", "StrangerApic0b57ec")
 API_BASE_URL = getenv("API_BASE_URL", "http://riyabots.site")
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Vip_Jani_OP")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+Fgru4laNvJpkMGI1")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/+YHI_piAspjlkODQ1")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+YHI_piAspjlkODQ1")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
