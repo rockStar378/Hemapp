720f39debe20464887f9281defa0602213eec51e
𝗜𝗦𝗧𝗞𝗛𝗔𝗥 𝗕𝗔𝗕𝗬 <mdistkhar601@gmail.com>
2026-01-03 20:54:59 +0530

    Update Youtube.py

--- RiyaMusic/platforms/Youtube.py
+++ RiyaMusic/platforms/Youtube.py
@@ -16,9 +16,9 @@ import requests
 import yt_dlp
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
-from py_yt import VideosSearch
-from ROCKYMUSIC.utils.database import is_on_off
-from ROCKYMUSIC.utils.formatters import time_to_seconds
+from youtubesearchpython.__future__ import VideosSearch
+from RiyaMusic.utils.database import is_on_off
+from RiyaMusic.utils.formatters import time_to_seconds
 import os
 import glob
 import random
